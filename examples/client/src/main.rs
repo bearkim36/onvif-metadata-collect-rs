10f67d9749b1888a080941981580c9b01eb12b2a
bearkim <bearkim@dkant.net>
2022-10-09 15:42:24 +0900

    first commit

---
+++ examples/client/src/main.rs
@@ -0,0 +1,77 @@
+// Copyright (C) 2022 bearkim forked by rertina
+
+use std::{str::FromStr, thread::Thread};
+use std::sync::Mutex;
+use std::collections::HashMap;
+use std::{thread, time};
+
+use actix_web::{get, middleware, rt, web, App, HttpRequest, HttpServer, HttpResponse, Responder, cookie::time::Duration};
+use lazy_static::lazy_static;
+
+mod metadata;
+use metadata::{Metadata, MetadataManager};
+
+
+// #[tokio::main(flavor = "multi_thread", worker_threads = 1000)]
+// async fn main() {    
+    
+//     let mut h = init_logging();        
+//     let _a = h.async_scope();
+//     for i in 1..20 {
+//         tokio::spawn(async move {
+//             run(i).await;    
+//         });            
+//     }
+//     loop{
+//         thread::sleep(time::Duration::from_millis(10));
+//     }
+// }
+lazy_static! {
+    static ref HASHMAP: Mutex<HashMap<u32, &'static str>> = {
+        let mut m = HashMap::new();
+        m.insert(0, "foo");
+        m.insert(1, "bar");
+        m.insert(2, "baz");
+        Mutex::new(m)
+    };        
+}
+
+#[get("/")]
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!\r\n"
+}
+
+#[get("/addMetadataConnect")]
+async fn add_metadata_connect() -> impl Responder {
+  
+    tokio::spawn(async move {                
+        let metadata = Metadata { 
+            url: String::from("rtsp://118.36.97.121/profile1/media.smp"),
+            username: String::from("admin"), 
+            password: String::from("!qaz2wsx"),
+            fclt_name: String::from("test"),
+        };
+        loop {
+            println!("start onvif session");
+            if let Err(_err) = metadata.run_onvif().await {
+                println!("retry 5sec after");
+                thread::sleep(time::Duration::from_secs(5));
+            }
+        }   
+    });            
+    HttpResponse::Ok().body("added Meatadata Connection")
+}
+
+#[tokio::main]
+async fn main()  -> std::io::Result<()> {    
+    
+
+    HttpServer::new(|| {
+        App::new().service(index).service(add_metadata_connect)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
---
+++ examples/client/src/metadata.rs
@@ -0,0 +1,83 @@
+use std::sync::Arc;
+use futures::StreamExt;
+use url::{Url};
+use anyhow::{anyhow, Error};
+
+use retina::codec::CodecItem;
+use retina::client::{SessionGroup, SetupOptions};
+use async_trait::async_trait;
+use uuid::Uuid;
+
+
+#[async_trait]
+pub trait MetadataManager {
+  async fn run_onvif(&self) -> Result<(), Error> ;
+}
+
+pub struct Metadata {  
+  pub url: String,  
+  pub username: String,  
+  pub password: String,
+  pub fclt_name: String,
+}
+
+
+#[async_trait]
+impl MetadataManager for Metadata {  
+  async fn run_onvif(&self) -> Result<(), Error> {
+    println!("onvif thread activation [{}]", self.fclt_name.clone());
+    println!("onvif url: {}", self.url.clone());
+    println!("onvif id/pw {}/{}", self.username.clone(), self.password.clone());    
+    let session_group = Arc::new(SessionGroup::default());
+    let creds = Some(retina::client::Credentials {
+        username : self.username.clone(),
+        password: self.password.clone(),
+    });
+    let mut session = retina::client::Session::describe(
+        Url::parse(self.url.as_str())?,
+        retina::client::SessionOptions::default()
+            .creds(creds)
+            .user_agent("Retina metadata example".to_owned())
+            .session_group(session_group),
+    )
+    .await?;
+    let onvif_stream_i = session
+        .streams()
+        .iter()
+        .position(|s| {
+            matches!(
+                s.parameters(),
+                Some(retina::codec::ParametersRef::Message(..))
+            )
+        })
+        .ok_or_else(|| anyhow!("couldn't find onvif stream"))?;
+    session
+        .setup(onvif_stream_i, SetupOptions::default())
+        .await?;
+    let mut session = session
+        .play(retina::client::PlayOptions::default().ignore_zero_seq(true))
+        .await?
+        .demuxed()?;
+
+ 
+    loop {
+        tokio::select! {
+            item = session.next() => {
+                match item.ok_or_else(|| anyhow!("EOF"))?? {
+                    CodecItem::MessageFrame(m) => {
+
+                      //  println!(
+                      //       "{}: {}\n",
+                      //       &m.timestamp(),
+                      //       std::str::from_utf8(m.data()).unwrap(),
+                      //   );
+                        
+                    },
+                    _ => continue,
+                };
+            },
+        }
+    }
+    
+  }
+}
