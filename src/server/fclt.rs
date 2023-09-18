7ccea95b54de958ffffbfd1d9a68e5d0563a7ddf
bearkim <bearkim@dkant.net>
2023-09-18 17:51:58 +0900

    aaaa

--- src/server/fclt.rs
+++ src/server/fclt.rs
@@ -1,7 +1,10 @@
 use mongodb::{bson::{doc, Bson}, bson, options::IndexOptions, Client, Collection, IndexModel, options::ClientOptions};
+use quickxml_to_serde::NullValue;
 use serde::{Deserialize, Serialize};
 use futures::TryStreamExt;
 use std::collections::HashMap;
+
+
 
 #[derive(Clone)]
 pub struct FcltLib {
@@ -87,12 +90,12 @@ impl FcltLib {
             let fd = FcltData {
                 fclt_id: fclt_models[i].fcltId.clone(),
                 fclt_name: fclt_models[i].fcltName.clone(),
-                camera_ip: camera_ip,
-                rtsp_port: rtsp_port,
-                camera_id: camera_id,
-                camera_pass: camera_pass,
-                ai_cam_model: ai_cam_model,
-                http_port: http_port
+                camera_ip,
+                rtsp_port,
+                camera_id,
+                camera_pass,
+                ai_cam_model,
+                http_port
             };
 
 
@@ -101,5 +104,68 @@ impl FcltLib {
         println!("{:?}", fclt_datas);
         
         fclt_datas
+    }
+
+    pub async fn get_fclt_one(&self, fclt_id:String) -> FcltData {            
+        let collection = self.db.collection::<FcltModel>("fclts");
+        let filter = doc! { "fcltId": fclt_id};
+        let response = collection.find(filter, None).await.unwrap();
+        
+        let fclt_models: Vec<FcltModel> = response.try_collect().await.unwrap();                
+
+        let mut fd = FcltData {
+            fclt_id: "".to_string(),
+            fclt_name: "".to_string(),
+            camera_ip: "".to_string(),
+            rtsp_port: "".to_string(),
+            camera_id: "".to_string(),
+            camera_pass: "".to_string(),
+            ai_cam_model: "".to_string(),
+            http_port: "".to_string()
+        };          
+
+        if fclt_models.len() > 0 {
+            println!("{:?}", fclt_models[0].fcltId);
+
+            let mut camera_ip:String = "".to_string();
+            let mut rtsp_port:String = "".to_string();
+            let mut camera_id:String = "".to_string();
+            let mut camera_pass:String = "".to_string();
+            let mut ai_cam_model:String = "".to_string();
+            let mut http_port:String = "80".to_string();
+
+            for j in 0..fclt_models[0].fcltTypeData.len() {                 
+                if fclt_models[0].fcltTypeData[j].dataKey.to_string().contains("cameraIp") {
+                    camera_ip = fclt_models[0].fcltTypeData[j].value.to_string().replace("\"", "");
+                }
+                else if fclt_models[0].fcltTypeData[j].dataKey.to_string().contains("rtspPort") {
+                    rtsp_port = fclt_models[0].fcltTypeData[j].value.to_string().replace("\"", "");
+                }                
+                else if fclt_models[0].fcltTypeData[j].dataKey.to_string().contains("cameraId") {
+                    camera_id = fclt_models[0].fcltTypeData[j].value.to_string().replace("\"", "");
+                }
+                else if fclt_models[0].fcltTypeData[j].dataKey.to_string().contains("cameraPass") {
+                    camera_pass = fclt_models[0].fcltTypeData[j].value.to_string().replace("\"", "");
+                }
+                else if fclt_models[0].fcltTypeData[j].dataKey.to_string().contains("aiCamModel") {
+                    ai_cam_model = fclt_models[0].fcltTypeData[j].value.to_string().replace("\"", "");
+                }
+                else if fclt_models[0].fcltTypeData[j].dataKey.to_string().contains("httpPort") {
+                    http_port = fclt_models[0].fcltTypeData[j].value.to_string().replace("\"", "");
+                }
+            }
+            
+            
+            fd.fclt_id= fclt_models[0].fcltId.clone();
+            fd.fclt_name= fclt_models[0].fcltName.clone();
+            fd.camera_ip= camera_ip;
+            fd.rtsp_port= rtsp_port;
+            fd.camera_id= camera_id;
+            fd.camera_pass= camera_pass;
+            fd.ai_cam_model= ai_cam_model;
+            fd.http_port= http_port;
+            
+        }
+        fd
     }
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -6,17 +6,31 @@
 #[macro_use]
 extern crate lazy_static;
 
+use async_once::AsyncOnce;
+use fclt::FcltData;
+
 use std::{thread, time, env};
-use std::sync::mpsc::{self, Receiver, Sender};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 use anyhow::{anyhow, Error};
 
-use rdkafka::config::ClientConfig;
+use rdkafka::client::ClientContext;
+use rdkafka::config::{ClientConfig, RDKafkaLogLevel};
 use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::consumer::stream_consumer::StreamConsumer;
+use rdkafka::consumer::{CommitMode, Consumer, ConsumerContext, Rebalance};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::{Headers, Message};
+use rdkafka::topic_partition_list::TopicPartitionList;
+use rdkafka::util::get_rdkafka_version;
 
 use tokio::task::JoinHandle;
+
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
 
 mod server_metadata;
 mod fclt;
@@ -26,145 +40,71 @@ extern crate dotenv;
 
 lazy_static! {
     static ref FCLT_DATA_MAP: Mutex<HashMap<String, server_metadata::MetadataConfig>> = Mutex::new(HashMap::new());
-    
-}
-pub struct TaskManager {
-    tasks: HashMap<String, Box<dyn ErasedJoinHandle>>,
-}
-
-trait ErasedJoinHandle {
-    fn abort(&self);
-}
-
-impl<T> ErasedJoinHandle for JoinHandle<T> {
-    fn abort(&self) {
-        JoinHandle::abort(self);
-    }
-}
-impl TaskManager {
-    pub fn abort_all(&mut self) {
-        for (_, handle) in self.tasks.drain() {
-            handle.abort();
-        }
-    }
-
-    pub fn abort(&mut self, key:String) {
-        for (ckey, handle) in self.tasks.drain() {
-            if ckey == key {
-                handle.abort();
-            }
-        }
-    }
-    
-    pub async fn add_task<T: 'static>(&mut self, key: String, handle: JoinHandle<T>) {        
-        self.tasks.insert(key, Box::new(handle));
-        
-        //tokio::join!(handle);
-    }
+    static ref TASK_MAP: Mutex<HashMap<String,JoinHandle<()>>> =  Mutex::new(HashMap::new());
+    static ref PRODUCER: AsyncOnce<FutureProducer> = AsyncOnce::new(async {        
+            let kafka_broker_1 = env::var("KAFKA_SERVER_BROKER_IP1").unwrap();                
+            ClientConfig::new()
+            .set("bootstrap.servers", kafka_broker_1)
+            .set("message.timeout.ms", "5000")
+            .set("security.protocol", "plaintext")
+            .create()
+            .expect("Producer creation error")        
+    });
 }
 
-async fn server_mode(mut task_manager:TaskManager) -> Result<(), Error> {
+
+#[derive(Serialize)]
+pub struct GenericResponse {
+    pub status: String,
+    pub message: String,
+}
+
+async fn server_mode() -> Result<(), Error> {
     let mongo_uri = env::var("MONGO_URI").unwrap();
-    let mongo_db_name = env::var("MONGO_DB_NAME").unwrap();
-    let kafka_broker_1 = env::var("KAFKA_SERVER_BROKER_IP1").unwrap();
-    
-    println!("MONGO_URI: {}", mongo_uri);
-    println!("MONGO_DB_NAME: {}", mongo_db_name);
-
-    let server_ip = String::from(env::var("SERVER_ADDR").unwrap_or("0.0.0.0".to_string()));    
-    let port : u16 = env::var("SERVER_PORT").unwrap_or("8000".to_string()).parse().unwrap();
+    let mongo_db_name = env::var("MONGO_DB_NAME").unwrap();    
     let fclt_obj = fclt::FcltLib::new(mongo_uri, mongo_db_name).await;
     let fclt_data = fclt_obj.get_fclt().await;
+    
+    for fd in fclt_data.clone().into_iter() {        
+        thread_proc(fd);        
+    }    
 
-    let mut producer: &FutureProducer = &ClientConfig::new()
-    .set("bootstrap.servers", kafka_broker_1)
-    .set("message.timeout.ms", "5000")
-    .set("security.protocol", "plaintext")
-    .create()
-    .expect("Producer creation error");
-            
-    let threads: Vec<_> = (0..fclt_data.len()).map(|i| {
-        let fd = fclt_data.to_owned();
-        let p = producer.to_owned();
-
-        let rtsp_id = &fd[i].camera_id;
-        let rtsp_pw = &fd[i].camera_pass;
-        let fclt_id = &fd[i].fclt_id;
-        let camera_ip = &fd[i].camera_ip;
-        let http_port = &fd[i].http_port;
-        let rtsp_port = &fd[i].rtsp_port;                
-
-        let ai_cam_model = &fd[i].ai_cam_model;
-        let img_save_path = String::from(env::var("IMG_SAVE_PATH").unwrap_or("./".to_string()));
-        let face_recognition_url = String::from(env::var("FACE_RECOGNITION_URL").unwrap_or("".to_string()));
-
-        let metadata_config = server_metadata::MetadataConfig { 
-            camera_ip: String::from(camera_ip),
-            http_port: String::from(http_port),
-            rtsp_port: String::from(rtsp_port),                    
-            username: String::from(rtsp_id), 
-            password: String::from(rtsp_pw),
-            fclt_id: String::from(fclt_id),
-            img_save_path,
-            ai_cam_model: String::from(ai_cam_model),
-            face_recognition_url,
-        };      
-
-        let mut _fclt_data_map = FCLT_DATA_MAP.lock().unwrap();
-        _fclt_data_map.insert(String::from(fclt_id), metadata_config.clone());
-
-        let handle = metadata_thread(metadata_config, p);                
-        
-        task_manager.add_task(String::from(fclt_id), handle);
-        
-
-        // handle
-    }).collect();
-
-    // for handle in threads {
-    //     tokio::join!(handle);        
-    // }
-
-    // let threads: Vec<_> = (0..fclt_data.len())
-    //     .map(|i| {                       
-    //         let fd = fclt_data.to_owned();
-    //         let p = producer.to_owned();
-
-            
-    //         metadata_thread(&metadata, &p)            
-    //         // tokio::spawn(async move {                                
-    //         //     // let rtsp_url = env::var("RTSP_URL").unwrap();
-    //         //     // let rtsp_id = env::var("RTSP_ID").unwrap();
-    //         //     // let rtsp_pw = env::var("RTSP_PW").unwrap();
-                
-                        
-
-    //         //     let mut manager = server_metadata::Manager {
-    //         //         is_receiving: false
-    //         //     };
-    //         //     loop {                          
-
-    //         //         if ai_cam_model.eq("") {
-    //         //             break;                        
-    //         //         }
-
-    //         //         println!("{} Start ONVIF session", i);
-    //         //         let _ = server_metadata::MetadataManager::run_onvif(&mut manager, metadata.clone(), p.clone()).await;
-                    
-    //         //         thread::sleep(time::Duration::from_secs(5));
-    //         //     }
-    //         // })
-    //     })
-    //     .collect();
-    loop {
-        thread::sleep(time::Duration::from_secs(5));
-    }
     Ok(())
 }
 
 
-fn metadata_thread(metadata_config:server_metadata::MetadataConfig, p:FutureProducer) -> tokio::task::JoinHandle<()> {
-    tokio::spawn( async move {        
+fn thread_proc(fd:FcltData) {
+
+    let rtsp_id = fd.camera_id;
+    let rtsp_pw = fd.camera_pass;
+    let fclt_id = fd.fclt_id;
+    let camera_ip = fd.camera_ip;
+    let http_port = fd.http_port;
+    let rtsp_port = fd.rtsp_port;                
+
+    let ai_cam_model = fd.ai_cam_model;
+    let img_save_path = String::from(env::var("IMG_SAVE_PATH").unwrap_or("./".to_string()));
+    let face_recognition_url = String::from(env::var("FACE_RECOGNITION_URL").unwrap_or("".to_string()));
+
+    let metadata_config = server_metadata::MetadataConfig { 
+        camera_ip: String::from(camera_ip),
+        http_port: String::from(http_port),
+        rtsp_port: String::from(rtsp_port),                    
+        username: String::from(rtsp_id), 
+        password: String::from(rtsp_pw),
+        fclt_id: String::from(fclt_id.clone()),
+        img_save_path,
+        ai_cam_model: String::from(ai_cam_model),
+        face_recognition_url,
+    };      
+    
+    let mut _fclt_data_map = FCLT_DATA_MAP.lock().unwrap();
+    if _fclt_data_map.contains_key(&fclt_id) {
+        _fclt_data_map.remove(&fclt_id);
+    }
+    _fclt_data_map.insert(String::from(fclt_id.clone()), metadata_config.clone());
+
+    let handle = tokio::spawn( async move {        
         let mut manager = server_metadata::Manager {
             is_receiving: true
         };
@@ -173,24 +113,122 @@ fn metadata_thread(metadata_config:server_metadata::MetadataConfig, p:FutureProd
             if metadata_config.ai_cam_model.eq("") {
                 break;                        
             }
-            
+            let p = PRODUCER.get().await;
             server_metadata::MetadataManager::run_onvif(&mut manager, metadata_config.clone(), p.clone()).await;
             
             thread::sleep(time::Duration::from_secs(5));
         }
-    })
+    });
+    let mut _task_map = TASK_MAP.lock().unwrap();
+    if _task_map.contains_key(&fclt_id) {
+        if let Some(task_handler) = _task_map.get(&fclt_id) {
+            task_handler.abort();
+        }
+        _task_map.remove(&fclt_id);
+    }
+    _task_map.insert(String::from(fclt_id), handle);                   
+}
+/*
+async fn kafka_consumer(mut task_manager:TaskManager) {    
+    let mut consumer = ClientConfig::new()
+        .set("group.id", "test-group")
+        .set("bootstrap.servers", "")
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "true")
+        //.set("statistics.interval.ms", "30000")
+        //.set("auto.offset.reset", "smallest")
+        .set_log_level(RDKafkaLogLevel::Debug)
+
+    consumer
+        .subscribe("spread")
+        .expect("Can't subscribe to specified topics");
+
+    loop {
+        match consumer.recv().await {
+            Err(e) => println!("Kafka error: {}", e),
+            Ok(m) => {
+                let payload = match m.payload_view::<str>() {
+                    None => "",
+                    Some(Ok(s)) => s,
+                    Some(Err(e)) => {
+                        println!("Error while deserializing message payload: {:?}", e);
+                        ""
+                    }
+                };
+                println!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+                if let Some(headers) = m.headers() {
+                    for header in headers.iter() {
+                        println!("  Header {:#?}: {:?}", header.key, header.value);
+                    }
+                }
+                consumer.commit_message(&m, CommitMode::Async).unwrap();
+            }
+        };
+    }
+}
+ */
+
+#[get("/api/test")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "Build Simple CRUD API with Rust and Actix Web";
+
+    let task = TASK_MAP.lock().unwrap();
+    if let Some(task_handler) = task.get("061a8bd0-a84b-11ed-abb7-27c66719279e") {
+        task_handler.abort();
+    }
+
+    let response_json = &GenericResponse {
+        status: "success".to_string(),
+        message: MESSAGE.to_string(),
+    };
+
+
+    HttpResponse::Ok().json(response_json)
 }
 
 
-#[tokio::main]
-async fn main() { 
+#[get("/api/test2")]
+async fn health_checker_handler2() -> impl Responder {
+    const MESSAGE: &str = "test2";
+    let mongo_uri = env::var("MONGO_URI").unwrap();
+    let mongo_db_name = env::var("MONGO_DB_NAME").unwrap();    
+    let fclt_obj = fclt::FcltLib::new(mongo_uri, mongo_db_name).await;
+    let fclt_data = fclt_obj.get_fclt_one("061a8bd0-a84b-11ed-abb7-27c66719279e".to_string()).await;    
+
+    thread_proc(fclt_data);
+
+    let response_json = &GenericResponse {
+        status: "success".to_string(),
+        message: MESSAGE.to_string(),
+    };
+
+
+    HttpResponse::Ok().json(response_json)
+}
+
+
+// #[tokio::main]
+#[actix_web::main]
+async fn main()  -> std::io::Result<()> { 
     let path = util::get_env_path().expect("Couldn't");    
+    let server_ip = String::from(env::var("SERVER_ADDR").unwrap_or("0.0.0.0".to_string()));    
+    let port : u16 = env::var("SERVER_PORT").unwrap_or("8000".to_string()).parse().unwrap();
+
     println!("{}", path.display());
-    dotenv::from_filename(path).expect("Failed to open directory");
-    let mut task_manager = TaskManager{ tasks: HashMap::new() };
+    dotenv::from_filename(path).expect("Failed to open directory");    
 
     println!("Boot on Server Mode");
-    server_mode(task_manager).await.unwrap();            
-    
+    server_mode().await.unwrap();            
+    HttpServer::new(move || {        
+        App::new()
+            .service(health_checker_handler)
+            .service(health_checker_handler2)
+            .wrap(Logger::default())
+    })
+    .bind((server_ip, port))?
+    .run()
+    .await
 }
 
